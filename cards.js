221b1033d0c3bf713de002902fe4a0fd877302fa
William Widjaja <wwidjaja0@gmail.com>
2025-04-16 20:06:32 -0700

    project setup; linked html with css and javascript files; third-party package cardsJS added

---
+++ cards.js
@@ -0,0 +1,17 @@
+
+// Create a representation of cards here
+
+
+// Create a representation of a deck or “stack” containing multiple cards here
+
+
+// Implement shuffling logic here
+
+
+// Implement a function to print "drew a card" to the console
+
+
+// Implement a function to print "shuffled the deck" to the console
+
+
+// Implement a functions to animate the cards here
