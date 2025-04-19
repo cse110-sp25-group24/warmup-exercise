3694570831dbaf1325bf951560cca3fe31d6b554
William Widjaja <62527551+wwidjaja0@users.noreply.github.com>
2025-04-18 17:06:47 -0700

    Revert "put in skeletons for shuffle and draw (issue 3)"

--- cards.js
+++ cards.js
@@ -1,18 +1,17 @@
 
 // Create a representation of cards here
 
+
 // Create a representation of a deck or “stack” containing multiple cards here
 
+
 // Implement shuffling logic here
-function shuffle() {
-    return console.log("Shuffle Logic");
-}
+
 
 // Implement a function to print "drew a card" to the console
-function draw() {
-    return console.log("Drew a new card")
-}
+
 
 // Implement a function to print "shuffled the deck" to the console
+
 
 // Implement a functions to animate the cards here
