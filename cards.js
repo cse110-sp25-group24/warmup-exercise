b0b24f33f188ec7e363b2b11c12fad7152f061a1
alexisvvega <a9vega@ucsd.edu>
2025-04-20 21:04:54 -0700

    fixed my mistake of drawing cards

--- cards.js
+++ cards.js
@@ -30,9 +30,14 @@ function drawCard() {
   number.textContent = card[0] + card[1];
   suit.textContent = card[card.length - 1];
 
-  const randomColor = Math.random() < 0.5 ? "red" : "black";
-  number.style.color = randomColor;
-  suit.style.color = randomColor;
+  if (suit.textContent == "♥" || suit.textContent == "♦") {
+    number.style.color = "red";
+    suit.style.color = "red";
+  }
+  else {
+    number.style.color = "black";
+    suit.style.color = "black";
+  }
 
   return card;
 }
