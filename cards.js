bdebb1e656119ab9d6e7a060384bea60cb52e499
Eric Song <e7song@ucsd.edu>
2025-04-20 19:43:56 -0700

    added in the following functionality: card shuffling, card drawing, displaying the cards being draw, displaying when the hand/deck are empty

--- cards.js
+++ cards.js
@@ -1,19 +1,34 @@
+let deck = [];
+let top_card = "";
+const deckBack = document.getElementById("deck");
+const pile = document.getElementById("pile");
+const drawButton = document.getElementById("draw");
+const number  = document.getElementById("number")
+const suit = document.getElementById("suit")
 
-deck = [];
-top_card = ""
-
+shuffleDeck();
 
 /**
  * Draws a card from the deck.
  * @returns {string} - The drawn card.
  */
-function drawCard(){
-  if (deck.length === 0) {
-    shuffleDeck();
-    return deck.pop();
+function drawCard() {
+  const card = deck.pop();
+
+  /* Functionality for showing that the deck is empty*/
+  if (deck.length == 0) {
+    deckBack.classList.replace('back-card', 'no-card');
+    drawButton.disabled = true;
   }
 
-  const card = deck.pop();
+  if (pile.classList.contains('no-card')) {
+    pile.classList.replace('no-card', 'card-front');
+  }
+
+  /* Functionality for showing what the top card is */
+  number.textContent = card[0];
+  suit.textContent = card[card.length - 1];
+
   return card;
 }
   
@@ -23,12 +38,12 @@ function drawCard(){
  * @returns {Array} - The shuffled deck of cards.
  */
 function shuffleDeck() {
-    const suits = ['H', 'D', 'C', 'S'];
+    const suits = ['♥', '♦', '♣', '♠'];
     const values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
     deck = [];
 
-    for (const i of suits){
-      for (const j of values){
+    for (const i of suits) {
+      for (const j of values) {
         deck.push(`${j} of ${i}`);
       }
     }
@@ -38,6 +53,18 @@ function shuffleDeck() {
   }
 
   console.log("Shuffled the deck!");
+  if (deckBack.classList.contains('no-card')) {
+    deckBack.classList.replace('no-card', 'back-card');
+    drawButton.disabled = false;
+  }
+
+  if (pile.classList.contains('card-front')) {
+    pile.classList.replace('card-front', 'no-card');
+    /* Integrate better w/ CSS next time*/
+    number.textContent = "";
+    suit.textContent = "";
+  }
+
   return deck;
 }
 
